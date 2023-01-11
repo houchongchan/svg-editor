ab12eff8d3de8ba91395b518a7dbfd9f2f9c4824
Hou <houchongchan@gmail.com>
2023-01-10 22:19:57 -0500

    update library

--- src/index.jsx
+++
@@ -1,5 +0,0 @@
-import ReactDOM from "react-dom";
-import "./globals.css";
-import App from "./components/App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
--- start.sh
+++
@@ -1,3 +0,0 @@
-npm run build
-npm install -g serve@13.0.4
-serve -s build
